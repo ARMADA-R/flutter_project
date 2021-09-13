c9f62e3f3dde58c384cfae9e8838a78e79d02d64
ARMADA <rafy199889@gmail.com>
2021-09-13 16:03:55 +0300

    rafi editing :)

---
+++ android/app/src/main/java/com/example/experienceapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.experienceapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,12 @@
+import 'package:experienceapp/screens/registeration_screen.dart';
+import 'package:flutter/material.dart';
+import '/screens/login_screen.dart';
+import '../screens/HomeScreen.dart';
+
+Map<String, WidgetBuilder>   routes =  <String, WidgetBuilder> {
+  LogInScreen.routeName : (context) => LogInScreen(title: 'LogInScreen',),
+  MySchool.routeName :(context) => MySchool(title: 'MySchool',),
+  MyRegistrationScreen.routeName :(context) => MyRegistrationScreen(title: 'MyRegistrationScreen',),
+
+
+};
