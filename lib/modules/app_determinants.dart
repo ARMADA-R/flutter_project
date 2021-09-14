79740b145fa31f7e89e0c1f40e10ae352f4fcda9
ARMADA <rafy199889@gmail.com>
2021-09-14 20:43:31 +0300

    rafi...

---
+++ lib/modules/app_determinants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppDeterminants with ChangeNotifier {
+
+Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+SharedPreferences? prefs ;
+ setLang(String lang) async{
+   SharedPreferences prefs = await _prefs;
+   prefs.setString('lang', lang);
+   notifyListeners();
+ }
+
+
+ get lang {
+   Future.delayed(Duration.zero, () async {
+    this.prefs = await _prefs;
+   });
+   return (prefs!.getString('lang')) ?? "en";
+ }
+
+
+}
