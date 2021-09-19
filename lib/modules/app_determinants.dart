715d07ed1299f5a70221fe84314304589e2b399d
ARMADA <rafy199889@gmail.com>
2021-09-19 21:08:37 +0300

    rafi edit..

--- lib/modules/app_determinants.dart
+++ lib/modules/app_determinants.dart
@@ -2,12 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppDeterminants with ChangeNotifier {
-
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  SharedPreferences? prefs;
 
   static String? _lang;
+  static String? _userId;
+  static String? _token;
+  static String? _loginAt;
+  static String? _schoolName;
   static bool? _lunchedForFirst;
-
 
   static final AppDeterminants _appDeterminants = AppDeterminants._internal();
 
@@ -16,51 +19,154 @@ class AppDeterminants with ChangeNotifier {
   }
   AppDeterminants._internal();
 
-  Future initializeAll() async{
+  Future initializeAll() async {
     await initializeIsLunchesStatus();
     await initializeLang();
-    await Future.delayed(Duration(milliseconds: 1500),);
+    await initializeToken();
+    await initializeUserId();
+    await initializeSchoolName();
+    initializeLoginAt();
+    await Future.delayed(
+      Duration(milliseconds: 400),
+    );
     return true;
-
   }
 
-
   setLang(String lang) async {
-    SharedPreferences prefs = await _prefs;
-    prefs.setString('lang', lang);
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    prefs!.setString('lang', lang);
     _lang = lang;
     notifyListeners();
   }
 
   setFirstLunch() async {
-    SharedPreferences prefs = await _prefs;
-    prefs.setBool('lunchedForFirst', true);
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    prefs!.setBool('lunchedForFirst', true);
     _lunchedForFirst = true;
     notifyListeners();
   }
 
+  setToken(String token) async {
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    prefs!.setString('token', token);
+    _token = token;
+    notifyListeners();
+  }
+
+  setUserId(String userId) async {
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    prefs!.setString('userId', userId);
+    _userId = userId;
+    notifyListeners();
+  }
+
+  setSchoolName(String schoolName) async {
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    prefs!.setString('schoolName', schoolName);
+    _schoolName = schoolName;
+    notifyListeners();
+  }
+
+  setLoginAt(String loginAt) async {
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    prefs!.setString('loginAt', loginAt);
+    _loginAt = loginAt;
+    notifyListeners();
+  }
+
+  String get loginAt {
+    if (_loginAt == null) {
+      _loginAt = "";
+    }
+    return _loginAt!;
+  }
+
   bool get isLunched {
-    if(_lunchedForFirst == null) {
+    if (_lunchedForFirst == null) {
       _lunchedForFirst = false;
     }
-      return _lunchedForFirst!;
+    return _lunchedForFirst!;
   }
 
   get lang {
-    if(_lang == null) {
+    if (_lang == null) {
       _lang = "en";
     }
-      return _lang;
+    return _lang;
   }
 
+  get token {
+    if (_token == null) {
+      _token = "en";
+    }
+    return _token;
+  }
+
+  get userId {
+    if (_userId == null) {
+      _userId = "en";
+    }
+    return _userId;
+  }
+
+  get schoolName {
+    if (_schoolName == null) {
+      _schoolName = "en";
+    }
+    return _schoolName;
+  }
 
   initializeIsLunchesStatus() async {
-    SharedPreferences prefs= await _prefs;
-      _lunchedForFirst = (prefs.getBool('lunchedForFirst')) ?? false;
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    _lunchedForFirst = (prefs!.getBool('lunchedForFirst')) ?? false;
   }
 
   initializeLang() async {
-    SharedPreferences prefs = await _prefs;
-      _lang = (prefs.getString('lang')) ?? "en";
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    _lang = (prefs!.getString('lang')) ?? "en";
+  }
+
+  initializeToken() async {
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    _token = (prefs!.getString('token')) ?? "";
+  }
+
+  initializeUserId() async {
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    _userId = (prefs!.getString('userId')) ?? "";
+  }
+
+  initializeSchoolName() async {
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    _schoolName = (prefs!.getString('schoolName')) ?? "";
+  }
+
+  initializeLoginAt() async {
+    if (prefs == null) {
+      prefs = await _prefs;
+    }
+    _loginAt = (prefs!.getString('loginAt')) ?? "";
   }
 }
