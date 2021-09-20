6bed8917805883a6b75fc8d0e42fe81707ab5819
ghinwa <ghinwaghassan2@gmail.com>
2021-09-20 13:44:18 +0300

    ghinwa

---
+++ lib/screens/Form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OurForm extends StatefulWidget {
+  @override
+  _OurFormState createState() => _OurFormState();
+}
+
+class _OurFormState extends State<OurForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(child: Text('')),
+    );
+  }
+}
